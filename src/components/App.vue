ca74f7d313fac6729e6ca1346a6191576ed7e676
chendalin666 <1255925603@qq.com>
2021-03-09 15:20:50 +0800

    first commit

--- src/components/App.vue
+++ src/components/App.vue
@@ -3,17 +3,10 @@
 <template>
     <div>
         <h1 v-html="msg"></h1>
-        <mt-button type="primary" @click="show">primary</mt-button>
-        <button type="button" class="mui-btn mui-btn-danger">红色</button>
-        <router-link to="/login">登录</router-link>
-        <router-link to="/reg">注册</router-link>
-        <router-view></router-view>
     </div>
 </template>
 
 <script>
-// 引入mintUI中的弹窗
-import { Toast } from 'mint-ui';
 export default {
     data() {
         return {
@@ -22,24 +15,12 @@ export default {
     },
     methods: {
         show(){
-            // 弹窗
-            Toast({
-                // 弹窗信息
-                message:'lbwnb',
-                // 弹窗位置
-                position:'top',
-                // 持续时间2秒
-                duration:2000,
-                // 设置弹窗的图标样式
-                iconClass:'bi-align-top msg'
-            })
+            
         }
     },
 }
 </script>
 
-// 使用style的scoped属性实现私有样式的定义
-// 原理是给当前组件的标签创建自定义属性data-xxxx
 <style lang="css" scoped>
     h1{
         color: rgb(0, 130, 130);
--- src/main.js
+++ src/main.js
@@ -1,21 +1,4 @@
 import Vue from 'vue'
-// 导入路由模块
-import VueRouter from 'vue-router'
-// 使用路由
-Vue.use(VueRouter)
-// 导入自己的路由模块，得到路由对象
-import router from './router.js'
-// 导入mint-ui
-import MintUI from 'mint-ui'
-import 'mint-ui/lib/style.css'
-Vue.use(MintUI)
-// 导入bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-// 导入mui
-import '../lib/mui/css/mui.min.css'
-// 导入全局样式
-import './css/global.css'
 // 导入App根组件
 import App from './components/App.vue'
 // 创建vue实例
@@ -28,7 +11,5 @@ const vm = new Vue({
 
     },
     // 用render渲染的组件是根组件
-    render:c=> c(App),
-    // 挂载路由
-    router
+    render:c=> c(App)
 })
